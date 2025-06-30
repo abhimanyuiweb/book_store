0242d1d478a9d9008bb2328b53d444929b41e957
Kumar Abhimanyu <abhiajad@gmail.com>
2025-07-01 00:12:55 +0530

    Linked CICD

--- server.js
+++ server.js
@@ -1,80 +1,11 @@
 const express = require('express')
 const app = express();
-const mongoose = require('mongoose');
-const aws = require('aws-sdk');
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-var nodemailer = require('nodemailer');
 
 console.log('Testing.....');
 
-// var dynamodb = new aws.DynamoDB({
-//     region: "us-east-1",
-//     apiVersion: "2012-08-10"
-//   });
-  
-// dynamodb.scan({
-//     TableName: "fruits"
-// }, function (err, data) {
-//   if (err) {
-//     console.log(err, err.stack); // an error occurred
-//   }
-//   else {
-//     let items = data.Items.map(item => {
-//         return {
-//             name: item.name.S,
-//             quantity: item.quantity.S
-//         };
-//     });
-//     console.log(items);           // successful response
-//   }
-// });
-
-
-// aws.config.getCredentials(function(err) {
-//     if (err) console.log(err.stack);
-//     // credentials not loaded
-//     else {
-//     //   console.log("Access key:", aws.config.credentials.secretAccessKey);
-//     }
-//   });
-
-
-// var transporter = nodemailer.createTransport({
-//     host: 'smtp.ethereal.email',
-//     port: 587,
-//     auth: {
-//         user: 'leon.schmitt98@ethereal.email',
-//         pass: 'gEv7NQtGyAEy91H723'
-//     }
-// });
-
-// var mailOptions = {
-//     from: 'jk@gmail.com',
-//     to: 'abhimanyuiweb@gmail.com',
-//     subject: 'Sending Email using Node.js',
-//     text: 'That was easy!'
-//   };
-
-const config = require("./config/config");
-const responses = require("./helpers/response");
-
 app.use(express.json());
-
-const Book = require("./models/book.model");
-const User = require("./models/user.model");
-
-
-var mongoDB = `mongodb://${config.HOST}/${config.DB}`;
-mongoose.connect(mongoDB, {useNewUrlParser: true});
-
-mongoose.connection
-.once('open', () => {
-    console.log("MongoDB Connected.");
-})
-.on("error", (error) => {
-    console.log("Your error:", error);
-})
 
 app.get('/', function (req, res) {
     // while(true) {
@@ -84,112 +15,53 @@ app.get('/', function (req, res) {
 })
 
 app.get('/books', (req, res) => {
-
-
-    // transporter.sendMail(mailOptions, function(error, info){
-    //     if (error) {
-    //       console.log(error);
-    //     } else {
-    //       console.log('Email sent: ' + info.response);
-    //     }
-    //   });
-    Book.find({}).then((books)=>{
-        if(books.length > 0){
-            return  responses.successResponseWithData(res, "Books found", books);
-        }else{
-            return responses.successResponseWithData(res, "No books found", []);
-        }
+    res.json({
+        name: 'Kumar ABhimanyu'
     });
 });
 
 app.post('/books', (req, res) => {
-    try {
-        var book = new Book(
-        { 
-            name: req.body.name,
-            author: req.body.author,
-            isbn: req.body.isbn
-        });
-
-        // TODO: data sanitization before saving, throw error if issue in data
-        //Save book.
-        book.save((err) => {
-            if (err) { 
-                return responses.validationErrorWithData(res, "Validation Error.", errors.array());
-            }
-
-            return responses.successResponseWithData(res, "Book added successfully.", book);
-        });
-    } catch (err) {
-        return responses.ErrorResponse(res, err);
-    }
+    
 });
 
 // A service to create user for testing without authentocation. Its not a good practive.
 // I am using for creating dummy users.
 app.post('/user', (req, res) => {
-    try {
-        User.findOne({email: req.body.email}).then((user)=> {
-            if (user) {
-                return responses.validationErrorWithData(res, "User already exists with email: " + user.email); 
-            }
-
-            // TODO: data sanitization before saving, throw error if issue in data
-            // Hash password and Save user.
-            bcrypt.hash(req.body.password, 10, function(err, hash) {
-                const user = new User(
-                { 
-                    name: req.body.name,
-                    email: req.body.email,
-                    password: hash
-                });
-
-                user.save((err) => {
-                    if (err) { 
-                        return responses.validationErrorWithData(res, "Validation Error.", errors.array());
-                    }
-        
-                    return responses.successResponseWithData(res, "User added successfully.", user);
-                });
-            })
-        });
-    } catch (err) {
-        return responses.ErrorResponse(res, err);
-    }
+    
 });
 
-app.post('/authentication', (req, res) => {
-    User.findOne({email: req.body.email}).then((user)=>{
-        if(user !== null) {
-            //Compare given password with db's hash.
-            bcrypt.compare(req.body.password, user.password,function (err, same) {
-                if (same) {
-                    let userData = {
-                        _id: user._id,
-                        name: user.name,
-                        email: user.email,
-                    };
-                    //Prepare JWT token for authentication
-                    const jwtPayload = userData;
-                    const jwtData = {
-                        expiresIn: config.JWT_TIMEOUT_DURATION,
-                    };
-                    const secret = config.JWT_SECRET;
-                    //Generated JWT token with Payload and secret.
-                    userData.token = jwt.sign(jwtPayload, secret, jwtData);
-                    return responses.successResponseWithData(res, "Authentication Success.", userData);
-                } else {
-                    return responses.unauthorizedResponse(res, "Email or Password wrong.");
-                }
-            });
-        }else {
-            return responses.successResponseWithData(res, "Invalid username or password", {});
-        }
-    });
-});
+// app.post('/authentication', (req, res) => {
+//     User.findOne({email: req.body.email}).then((user)=>{
+//         if(user !== null) {
+//             //Compare given password with db's hash.
+//             bcrypt.compare(req.body.password, user.password,function (err, same) {
+//                 if (same) {
+//                     let userData = {
+//                         _id: user._id,
+//                         name: user.name,
+//                         email: user.email,
+//                     };
+//                     //Prepare JWT token for authentication
+//                     const jwtPayload = userData;
+//                     const jwtData = {
+//                         expiresIn: config.JWT_TIMEOUT_DURATION,
+//                     };
+//                     const secret = config.JWT_SECRET;
+//                     //Generated JWT token with Payload and secret.
+//                     userData.token = jwt.sign(jwtPayload, secret, jwtData);
+//                     return responses.successResponseWithData(res, "Authentication Success.", userData);
+//                 } else {
+//                     return responses.unauthorizedResponse(res, "Email or Password wrong.");
+//                 }
+//             });
+//         }else {
+//             return responses.successResponseWithData(res, "Invalid username or password", {});
+//         }
+//     });
+// });
 
-app.listen(config.PORT, () => {
-    console.log(`Expres server listening on: ${config.PORT}`);
+app.listen(3000, () => {
+    console.log(`Expres server listening on: 3000`);
 });
 
 
